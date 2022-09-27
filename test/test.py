d9499534e35eec724e996abb9cb30a64c1ceb293
Tord Nytun <tord.nytun@student.uib.no>
2022-09-27 16:33:03 +0200

    fix typo in testfile

--- test/test.py
+++ test/test.py
@@ -9,7 +9,7 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(sumtwointegers(1, 1), 2)
 
     def test_multiplytwointegers(self):
-        self.assert_(multiplytwointegers(2, 2), 4)
+        self.assertEquals(multiplytwointegers(2, 2), 4)
 
 
 if __name__ == '__main__':
