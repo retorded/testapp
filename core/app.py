b4fd7a4644e7e19dcc5b338d7c87a811a19622e4
Tord Nytun <tord.nytun@student.uib.no>
2022-09-27 16:28:15 +0200

    add functions to app and tests

--- core/app.py
+++ core/app.py
@@ -1,7 +1,22 @@
-def sumTwoInts(a, b):
+def sumtwointegers(a, b):
 
     # typecheck
-    if (isinstance(a, int) and isinstance(b, int)):
+    if isinstance(a, int) and isinstance(b, int):
         return a + b
     else:
         raise ValueError("Input not ints")
+
+def multiplytwointegers(a, b):
+
+    # typecheck
+    if isinstance(a, int) and isinstance(b, int):
+        return a*b
+    else:
+        raise ValueError("Input not ints")
+
+def isaninteger(a):
+    # typecheck
+    if isinstance(a, int):
+        return True
+    else:
+        return False
--- test/test.py
+++ test/test.py
@@ -5,8 +5,11 @@ from core.app import *
 
 class MyTestCase(unittest.TestCase):
 
-    def test_sumTwoInts(self):
-        self.assertEqual(sumTwoInts(1, 1), 2)
+    def test_sumtwointegers(self):
+        self.assertEqual(sumtwointegers(1, 1), 2)
+
+    def test_multiplytwointegers(self):
+        self.assert_(multiplytwointegers(2, 2), 4)
 
 
 if __name__ == '__main__':
